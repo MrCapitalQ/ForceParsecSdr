373b00bdbb8d034275535669cc4bf6108f765bcf
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2024-03-25 22:37:33 -0400

    Configure app to be single instance.

---
+++ src/MrCapitalQ.ForceParsecSdr/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Microsoft.Windows.AppLifecycle;
+using MrCapitalQ.ForceParsecSdr;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    private const string AppKey = "943116d4-f8a9-41d2-bc03-97049f87886a";
+
+    [STAThread]
+    private static async Task Main()
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+
+        var keyInstance = AppInstance.FindOrRegisterForKey(AppKey);
+        if (!keyInstance.IsCurrent)
+        {
+            var activationArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
+            await keyInstance.RedirectActivationToAsync(activationArgs);
+            return;
+        }
+
+        Application.Start((p) =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(
+                DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            _ = new App();
+        });
+    }
+}
